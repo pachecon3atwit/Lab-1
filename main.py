ee3c9514f44ef434d2a59b8bbdfc6fe13f2d8cfa
pachecon3atwit <pachecon3@wit.edu>
2025-05-28 14:17:26 -0400

    Updated code

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+from email.policy import default
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/Soccer")
+@app.get("/")
 async def read_root():
     return {"message": "Welcome to the Soccer API"}
+
+@app.get("/player_goals")
+async def player_goals(name: str, goals: int):
+    return f"Player name: {name} | Goals this season: {goals}"
+
+
+
+@app.get("/team_info/{team_name}")
+async def team_info(team_name: str):
+    return f"{team_name} is a legendary football club!"
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
